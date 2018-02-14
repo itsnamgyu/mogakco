05d112968444c9c87995e0358a4a708c6acdcdf1
itsnamgyu <itsnamgyu@gmail.com>
2018-02-14 15:57:30 +0000

    fixed settings files

--- mogakco/settings/production.py
+++ mogakco/settings/production.py
@@ -1,7 +1,4 @@
-from .settings import *
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['mogak.co']
 
 STATIC_ROOT = "/var/www/mogak.co/static/"
